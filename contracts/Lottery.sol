bbc667e2f025524cdf5cd4b81bd739562dad7f34
Nasyx Rakeeb <nasyxrakeeb2@gmail.com>
2022-10-10 20:13:00 +0530

    Add functions

---
+++ contracts/Lottery.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.17;
+
+contract Lottery {
+    address public manager;
+    address[] public players;
+    
+    function Lottery() public {
+        manager = msg.sender;
+    }
+    
+    function enter() public payable {
+        players.push(msg.sender);
+    }
+}
