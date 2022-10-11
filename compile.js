a6d26c78a0b080b1e189f34f1c59b7e1d51b2b84
Nasyx Rakeeb <nasyxrakeeb2@gmail.com>
2022-10-11 18:15:18 +0530

    add

--- compile.js
+++ compile.js
@@ -3,6 +3,6 @@ const path = require('path')
 const solc = require('solc')
 
 const lotteryPath = path.resolve(__dirname, "contracts", "Lottery.sol")
-const source = fs.readFileSync(inboxPath, "utf8")
+const source = fs.readFileSync(lotteryPath, "utf8")
 
 module.exports = solc.compile(source, 1).contracts[":Lottery"]
