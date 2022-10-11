afc2697eb1317d3cecb2457a992c80aae7869add
Nasyx Rakeeb <nasyxrakeeb2@gmail.com>
2022-10-11 18:41:19 +0530

    add

--- test/lottery.test.js
+++ test/lottery.test.js
@@ -28,6 +28,26 @@ describe("Lottery Contract", () => {
     assert.equal(1, players.length)
     assert.equal(accounts[0], players[0])
   })
+  
+  it("allows one account entry", async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei("0.0001", "ether")
+    })
+    await lottery.methods.enter().send({
+      from: accounts[1],
+      value: web3.utils.toWei("0.0001", "ether")
+    })
+    await lottery.methods.enter().send({
+      from: accounts[2],
+      value: web3.utils.toWei("0.0001", "ether")
+    })
+    const players = await lottery.methods.getPlayers().call({from: accounts[0]})
+    assert.equal(3, players.length)
+    assert.equal(accounts[0], players[0])
+    assert.equal(accounts[1], players[1])
+    assert.equal(accounts[2], players[2])
+  })
 })
 
 
